83fc678578d8980d7290c230482611f62fe3ccf4
Abdeslam <abdeslam9@gmail.com>
2016-07-23 11:45:56 +0100

    Add and config acts_as_votable

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
 	def index
 		@pins = Pin.all.order("created_at DESC")
 	end
@@ -22,17 +22,22 @@ class PinsController < ApplicationController
 			redirect_to @pin, notice: "Pin was Successfuly updated!"
 		else
 			render 'edit'
-		end
-		
+		end	
 	end
+
+	def show	
+	end
+	
 	def destroy
 		@pin.destroy
 		redirect_to root_path
-		
 	end
-	def show
-		
+
+	def upvote
+		@pin.upvote_by current_user
+		redirect_to :back
 	end
+
 
 	private
 	
